caa999a746e476d0de58fe8ee95cfcf00b99082d
George Salu <george.salu10@gmail.com>
2021-01-15 11:29:27 -0300

    configurations e beans

--- src/main/java/com/example/vendas/VendasApplication.java
+++ src/main/java/com/example/vendas/VendasApplication.java
@@ -1,5 +1,7 @@
 package com.example.vendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+	
+	@Autowired
+	@Qualifier("applicationName")
+	private String applicationName;
 
 	@GetMapping("/hello")
 	public String helloWorld() {
