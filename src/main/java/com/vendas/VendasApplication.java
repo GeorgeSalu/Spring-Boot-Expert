f9f860cdb505dbc94b093adcecda296d1b981393
George Salu <george.salu10@gmail.com>
2021-01-15 11:46:10 -0300

    o container ioc de spring framework

--- src/main/java/com/vendas/VendasApplication.java
+++ src/main/java/com/vendas/VendasApplication.java
@@ -4,11 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@ComponentScan(
+		basePackages = {
+				"com.vendas.repository",
+				"com.vendas.service"
+		}
+)
 public class VendasApplication {
 	
 	@Autowired
---
+++ src/main/java/com/vendas/service/MeuServico.java
@@ -0,0 +1,8 @@
+package com.vendas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeuServico {
+
+}
