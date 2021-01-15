06ce055a2b1a55722f05dcc99b53a17a77bd8578
George Salu <george.salu10@gmail.com>
2021-01-15 11:41:34 -0300

    modificando estrutura de pacotes

--- src/main/java/com/vendas/MinhaConfiguration.java
+++ src/main/java/com/vendas/MinhaConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.vendas;
+package com.vendas;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/vendas/VendasApplication.java
+++ src/main/java/com/vendas/VendasApplication.java
@@ -1,4 +1,4 @@
-package com.example.vendas;
+package com.vendas;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
