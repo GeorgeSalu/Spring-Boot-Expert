3f5b4e2ac27b6a01c8fe71bc5b42fd18589b937f
George Salu <george.salu10@gmail.com>
2021-01-15 12:10:54 -0300

    configurações externalizada: application properties

--- src/main/java/com/vendas/MinhaConfiguration.java
+++ src/main/java/com/vendas/MinhaConfiguration.java
@@ -1,14 +1,9 @@
 package com.vendas;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinhaConfiguration {
 
-	@Bean(name = "applicationName")
-	public String applicationName() {
-		return "Sistema de vendas";
-	}
 	
 }
