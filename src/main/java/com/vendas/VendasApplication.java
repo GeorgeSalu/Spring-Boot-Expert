651546c56ef9f7712526b7ee5bcea6f31da67c0f
George Salu <george.salu10@gmail.com>
2021-01-15 15:03:16 -0300

    salvando cliente - jdbcTemplate

--- src/main/java/com/vendas/VendasApplication.java
+++ src/main/java/com/vendas/VendasApplication.java
@@ -1,13 +1,28 @@
 package com.vendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vendas.entity.Cliente;
+import com.vendas.repository.ClienteRepository;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
+	@Bean
+	public CommandLineRunner init(@Autowired ClienteRepository clienteRepository) {
+		return args -> {
+			Cliente cliente = new Cliente();
+			cliente.setNome("douglas");
+			clienteRepository.salvar(cliente);
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
