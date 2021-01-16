c85940eb5bdf3c1e8eca9434da5749fa6a573bc2
George Salu <george.salu10@gmail.com>
2021-01-15 22:09:15 -0300

    mapeando a entidade cliente para jpa

--- src/main/java/com/vendas/entity/Cliente.java
+++ src/main/java/com/vendas/entity/Cliente.java
@@ -1,11 +1,25 @@
 package com.vendas.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
+	
+	@Column(name = "nome", length = 100)
 	private String nome;
 
 	public Cliente() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	public Cliente(Integer id, String nome) {
