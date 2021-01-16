eb491258a306bdf5542740c884131532b610ab68
George Salu <george.salu10@gmail.com>
2021-01-15 22:46:30 -0300

    removendo referencias ao jdbcTemplate

--- src/main/java/com/vendas/repository/ClienteRepository.java
+++ src/main/java/com/vendas/repository/ClienteRepository.java
@@ -1,15 +1,11 @@
 package com.vendas.repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,14 +14,6 @@ import com.vendas.entity.Cliente;
 @Repository
 public class ClienteRepository {
 
-	private static String INSERT = "insert into cliente (nome) values (?)";
-	private static String SELECT_ALL = "select * from cliente";
-	private static String UPDATE = "update cliente set nome = ? where id = ?";
-	private static String DELETE = "delete from cliente where id = ?";
-	
-	@Autowired
-	private JdbcTemplate jdbcTemplate; 
-	
 	@Autowired
 	private EntityManager entityManager;
 	
