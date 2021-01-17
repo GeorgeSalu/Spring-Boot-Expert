c70e67d4eac83f24390db9db2df44bfb7ed0c8ed
George Salu <george.salu10@gmail.com>
2021-01-17 15:22:37 -0300

    fazendo consultas com relacionamentos jpa

--- src/main/java/com/vendas/VendasApplication.java
+++ src/main/java/com/vendas/VendasApplication.java
@@ -1,6 +1,7 @@
 package com.vendas;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,40 +11,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vendas.entity.Cliente;
+import com.vendas.entity.Pedido;
 import com.vendas.repository.ClientesRepository;
+import com.vendas.repository.PedidosRepository;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
 	@Bean
-	public CommandLineRunner init(@Autowired ClientesRepository clienteRepository) {
+	public CommandLineRunner init(@Autowired ClientesRepository clienteRepository,
+			@Autowired PedidosRepository pedidosRepository) {
 		return args -> {
 			Cliente cliente = new Cliente();
 			cliente.setNome("douglas");
 			clienteRepository.save(cliente);
 			
-			Cliente outroCliente = new Cliente();
-			outroCliente.setNome("douglas 2");
-			clienteRepository.save(outroCliente);
+			Pedido p = new Pedido();
+			p.setCliente(cliente);
+			p.setDataPedido(LocalDate.now());
+			p.setTotal(BigDecimal.valueOf(1000));
 			
-			
-			List<Cliente> todosClientes = clienteRepository.findAll();
-			todosClientes.forEach(System.out::println);
-			
-			todosClientes.forEach(c -> {
-				c.setNome(c.getNome() + " atualizado");
-				clienteRepository.save(c);
-			});
-			
-			todosClientes = clienteRepository.findAll();
-			todosClientes.forEach(System.out::println);
-			
-			boolean existsByNome = clienteRepository.existsByNome("douglas");
-			System.out.println(existsByNome);
-			
-			List<Cliente> encontrarPorNome = clienteRepository.encontrarPorNome("douglas atualizado");
-			encontrarPorNome.forEach(System.out::println);
+			pedidosRepository.save(p);
+
+			Cliente clientePedidos = clienteRepository.findClienteFetchPedidos(cliente.getId());
+			System.out.println(clientePedidos);
 		};
 	}
 	
--- src/main/java/com/vendas/repository/ClientesRepository.java
+++ src/main/java/com/vendas/repository/ClientesRepository.java
@@ -23,4 +23,9 @@ public interface ClientesRepository extends JpaRepository<Cliente, Integer> {
 	List<Cliente> findByNomeOrId(String nome, Integer id);
 	
 	boolean existsByNome(String nome);
+	
+	@Query(value = "select c from Cliente c left join fetch c.pedidos where c.id = :id ")
+	Cliente findClienteFetchPedidos(@Param("id") Integer id);
+	
+	
 }
