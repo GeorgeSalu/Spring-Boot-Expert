02d61265cedcc09bbec85cb4c261649399627532
George Salu <george.salu10@gmail.com>
2021-01-17 15:25:58 -0300

    corrigindo mapeamento de Pedido

--- src/main/java/com/vendas/entity/Pedido.java
+++ src/main/java/com/vendas/entity/Pedido.java
@@ -29,7 +29,7 @@ public class Pedido {
 	@Column(name = "data_pedido")
 	private LocalDate dataPedido;
 
-	@Column(name = "total", length = 20, precision = 2)
+	@Column(name = "total", scale = 2, precision = 20)
 	private BigDecimal total;
 
 	@OneToMany(mappedBy = "pedido")
