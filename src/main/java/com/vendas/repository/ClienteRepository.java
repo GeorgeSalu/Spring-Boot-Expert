2b47f3663b092edaaaee61bb7588abeb93deb8a9
George Salu <george.salu10@gmail.com>
2021-01-17 13:19:30 -0300

    introdução aos repositorios spring data

--- src/main/java/com/vendas/repository/ClienteRepository.java
+++ src/main/java/com/vendas/repository/ClienteRepository.java
@@ -1,9 +1,12 @@
 package com.vendas.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vendas.entity.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	List<Cliente> findByNomeLike(String nome);
 }
