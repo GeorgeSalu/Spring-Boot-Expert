f908a14e49c791e5664b6cf3a00a904176d22353
George Salu <george.salu10@gmail.com>
2021-01-17 15:40:37 -0300

    criando o controller de clientes

--- src/main/java/com/vendas/VendasApplication.java
+++ src/main/java/com/vendas/VendasApplication.java
@@ -1,44 +1,13 @@
 package com.vendas;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.vendas.entity.Cliente;
-import com.vendas.entity.Pedido;
-import com.vendas.repository.ClientesRepository;
-import com.vendas.repository.PedidosRepository;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
-	@Bean
-	public CommandLineRunner init(@Autowired ClientesRepository clienteRepository,
-			@Autowired PedidosRepository pedidosRepository) {
-		return args -> {
-			Cliente cliente = new Cliente();
-			cliente.setNome("douglas");
-			clienteRepository.save(cliente);
-			
-			Pedido p = new Pedido();
-			p.setCliente(cliente);
-			p.setDataPedido(LocalDate.now());
-			p.setTotal(BigDecimal.valueOf(1000));
-			
-			pedidosRepository.save(p);
-
-			Cliente clientePedidos = clienteRepository.findClienteFetchPedidos(cliente.getId());
-			System.out.println(clientePedidos);
-		};
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
