3d98032a1382a78d871726a66259cc65ef204bb2
George Salu <george.salu10@gmail.com>
2021-01-17 14:12:59 -0300

    mapeando a entidade pedido

--- src/main/java/com/vendas/entity/Cliente.java
+++ src/main/java/com/vendas/entity/Cliente.java
@@ -1,10 +1,13 @@
 package com.vendas.entity;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +22,9 @@ public class Cliente {
 	@Column(name = "nome", length = 100)
 	private String nome;
 
+	@OneToMany(mappedBy = "cliente")
+	private Set<Pedido> pedidos;
+	
 	public Cliente() {
 	}
 	
