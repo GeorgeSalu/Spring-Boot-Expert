9418880cb772ceab939d43913fa6e662b7d29757
George Salu <george.salu10@gmail.com>
2021-01-18 14:58:17 -0300

    removendo imports não utilizados

--- src/main/java/com/vendas/controller/ProdutoController.java
+++ src/main/java/com/vendas/controller/ProdutoController.java
@@ -2,7 +2,14 @@ package com.vendas.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
+import java.util.List;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.vendas.entity.Cliente;
 import com.vendas.entity.Produto;
 import com.vendas.repository.ProdutosRepository;
 
@@ -44,5 +52,36 @@ public class ProdutoController {
 			}).orElseThrow(() -> 
 				new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado")
 			);
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Integer id) {
+		produtosRepository.findById(id)
+			.map(p -> {
+				produtosRepository.delete(p);
+				return Void.TYPE;
+			}).orElseThrow(() -> 
+				new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado")
+			);
+	}
+	
+	@GetMapping("{id}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public Produto getById(@PathVariable Integer id) {
+		return produtosRepository.findById(id)
+				.orElseThrow(() -> 
+				new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado")
+			);
+	}
+	
+	@GetMapping
+	public List<Produto> find(Produto filtro) {
+		ExampleMatcher matcher = ExampleMatcher
+									.matching()
+									.withIgnoreCase()
+									.withStringMatcher(StringMatcher.CONTAINING);
+		Example example = Example.of(filtro, matcher);
+		return produtosRepository.findAll(example);
 	}
 }
