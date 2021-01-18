25d74e95d9804e8707b50959035114c614d0276f
George Salu <george.salu10@gmail.com>
2021-01-17 21:32:15 -0300

    requisicao get com paramentros e responseEntity

--- src/main/java/com/vendas/VendasApplication.java
+++ src/main/java/com/vendas/VendasApplication.java
@@ -1,12 +1,26 @@
 package com.vendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vendas.entity.Cliente;
+import com.vendas.repository.ClientesRepository;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired ClientesRepository clientesRepository) {
+		return args -> {
+			Cliente cliente = new Cliente(null, "Fulano");
+			clientesRepository.save(cliente);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
--- src/main/java/com/vendas/controller/ClienteController.java
+++ src/main/java/com/vendas/controller/ClienteController.java
@@ -1,28 +1,34 @@
 package com.vendas.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.vendas.entity.Cliente;
+import com.vendas.repository.ClientesRepository;
 
 @Controller
 public class ClienteController {
 
+	private ClientesRepository clientesRepository;
+
+	public ClienteController(ClientesRepository clientesRepository) {
+		this.clientesRepository = clientesRepository;
+	}
 	
-	@RequestMapping(
-			value = {"api/clientes/hello/{nome}", "api/hello"}, 
-			method = RequestMethod.POST,
-			consumes = {"application/json", "application/xml"},
-			produces = {"application/json", "application/xml"}
-	)
+	@GetMapping("/api/clientes/{id}")
 	@ResponseBody
-	public String helloClientes(@PathVariable("nome") String nomeCliente,
-								@RequestBody Cliente cliente) {
-		return String.format("Hello %s", nomeCliente);
+	public ResponseEntity<Cliente> helloClientes(@PathVariable Integer id) {
+		Optional<Cliente> cliente = clientesRepository.findById(id);
+		
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 }
