1e26608c1aff5332a0bcdfd03abd39ec97e8831f
George Salu <george.salu10@gmail.com>
2021-01-20 09:24:24 -0300

    utilizando e testando o bean validation

--- src/main/java/com/vendas/controller/ApplicationControllerAdvice.java
+++ src/main/java/com/vendas/controller/ApplicationControllerAdvice.java
@@ -1,6 +1,10 @@
 package com.vendas.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,5 +27,15 @@ public class ApplicationControllerAdvice {
 	@ResponseStatus(value = HttpStatus.NOT_FOUND)
 	public ApiErrors handlePedidoNotDoundException(PedidoNaoEncontradoException exception) {
 		return new ApiErrors(exception.getMessage());
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex) {
+		List<String> errors = ex.getBindingResult().getAllErrors()
+			.stream()
+			.map(erro -> erro.getDefaultMessage())
+			.collect(Collectors.toList());
+		return new ApiErrors(errors);
 	}
 }
