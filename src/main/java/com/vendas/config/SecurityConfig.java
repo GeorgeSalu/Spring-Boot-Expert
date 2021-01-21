a26a8391f000054008f1af63a4bcaa7f9ecad690
George Salu <george.salu10@gmail.com>
2021-01-20 22:00:26 -0300

    implementacao do userDetailsService

--- src/main/java/com/vendas/config/SecurityConfig.java
+++ src/main/java/com/vendas/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vendas.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.vendas.service.impl.UsuarioServiceImpl;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UsuarioServiceImpl usuarioSerive;
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -18,11 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		  auth.inMemoryAuthentication()
-		          .passwordEncoder(passwordEncoder())
-		          .withUser("fulano")
-		          .password(passwordEncoder().encode("123"))
-		          .roles("USER","ADMIN");
+		  auth
+		  	.userDetailsService(usuarioSerive)
+		  	.passwordEncoder(passwordEncoder());
 	}
 	
 	@Override
